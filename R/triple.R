0bd00cfb38ad769f592e95aac54c4ebfa8b8e512
Eric Marcon <e.marcon@free.fr>
2020-11-04 22:02:26 +0100

    Triple

--- R/triple.R
+++ R/triple.R
@@ -1,14 +1,49 @@
-# Méthode générique
+#' Triple
+#'
+#' A method to multiply numbers by 3.
+#'
+#' @param x 	an object used to select a method.
+#' @param ... further arguments passed to or from other methods.
+#'
+#' @export
 triple <- function(x, ...) {
   UseMethod("triple")
 }
 
 
+#' Triple of integer vectors
+#'
+#' This function multiplies the vector by 3.
+#'
+#' @param x an integer vector.
+#' @param ... ignored.
+#'
+#' @return an integer vector.
+#'
+#' @method triple integer
+#' @export
+#'
+#' @examples
+#' triple(1L:3L)
 triple.integer <- function(x, ...) {
   return(x * 3L)
 }
 
 
+#' Triple of numeric vectors
+#'
+#' This function multiplies the vector by 3.
+#'
+#' @param x a numeric vector.
+#' @param ... ignored.
+#'
+#' @return a numeric vector.
+#'
+#' @method triple numeric
+#' @export
+#'
+#' @examples
+#' triple(1:3)
 triple.numeric <- function(x, ...) {
   return(x * 3)
 }
